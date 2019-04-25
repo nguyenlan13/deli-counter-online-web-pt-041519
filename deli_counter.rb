315573bead6c7791866d5290c7402d71ab53b204
Lan Nguyen <nguyenlan13@gmail.com>
2019-04-25 08:14:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,8 @@ def line (deli)
     deli.empty
     puts "The line is currently empty."
   else
+    loop katz_deli do
+    
     current_place = "The line is currently: "
     deli.each_with_index(1) do |array,name|
     current_place << " #{name}. #{array}"
