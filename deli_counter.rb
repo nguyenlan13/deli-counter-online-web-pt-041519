33aac0120f9370181b65122f97218fa00dd4214c
Lan Nguyen <nguyenlan13@gmail.com>
2019-04-25 08:42:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,7 +24,7 @@ def now_serving
     deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{name}."
+    puts "Currently serving #{deli.first}."
   end
   deli.unshift
 end
