e776227032599b7a30c86f149a68e4d0349d0716
Lan Nguyen <nguyenlan13@gmail.com>
2019-04-25 08:15:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,10 @@
 
 
 def line (deli)
-  katz_deli = []
   if
     deli.empty
     puts "The line is currently empty."
   else
-    loop katz_deli do
     
     current_place = "The line is currently: "
     deli.each_with_index(1) do |array,name|
