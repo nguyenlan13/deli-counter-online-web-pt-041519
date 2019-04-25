3fbaf07da2d5ac44b410865c8524576533ff9cfe
Lan Nguyen <nguyenlan13@gmail.com>
2019-04-25 08:20:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line (deli)
   else
     current_place = "The line is currently:" 
     deli.each_with_index(1) do |name, i|
-    current_place << " #{i}. #{name}"
+    current_place << "#{i}. #{name}"
     end
     puts current_place
   end
